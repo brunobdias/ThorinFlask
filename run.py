24142fe62f372c2990a114f038fc33d69f2b7b7c
brunobdias <devbrunobdias@gmail.com>
2021-06-16 09:40:23 +0000

    Second part of commit, of whole Flask procject

---
+++ run.py
@@ -0,0 +1,32 @@
+import os
+import json
+from flask import Flask, render_template
+
+app = Flask(__name__) 
+"""__name__ builtin python variable"""
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company=data)
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html", page_title="Contact")
+
+@app.route("/carrers")
+def carrers():
+    return render_template("carrers.html", page_title="Carrers")
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True
+    )
